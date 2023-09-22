1a43fd4ee54aa63f0776414e58441c390e6e05df
Jesse Purcell <slaydevil@hotmail.com>
2023-09-22 10:23:56 +1000

    Refactor password check to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,7 +1,21 @@
 PASSWORD_MIN_LENGTH = 6
 
-password = input('Please enter a password: ')
-while len(password) < PASSWORD_MIN_LENGTH:
-    print(f'Password must be a minimum of {PASSWORD_MIN_LENGTH} characters')
+
+def main():
+    password = get_password()
+    print_password(password)
+
+
+def print_password(password):
+    print('*' * len(password))
+
+
+def get_password():
     password = input('Please enter a password: ')
-print('*' * len(password))
+    while len(password) < PASSWORD_MIN_LENGTH:
+        print(f'Password must be a minimum of {PASSWORD_MIN_LENGTH} characters')
+        password = input('Please enter a password: ')
+    return password
+
+
+main()
