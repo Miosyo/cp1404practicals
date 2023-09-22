d4aa597b8c553b8381e27f7f6793de50e82ab41e
Jesse Purcell <slaydevil@hotmail.com>
2023-09-22 10:56:04 +1000

    Add score menu

---
+++ prac_02/score_menu.py
@@ -0,0 +1,43 @@
+from prac_02.score import determine_score
+
+
+def main():
+    score = get_valid_score()
+    print_menu()
+    choice = input('').upper()
+    while choice != 'Q':
+        if choice == 'G':
+            score = get_valid_score()
+        elif choice == 'P':
+            print_result(score)
+        elif choice == 'S':
+            print_stars(score)
+        else:
+            print('Invalid choice')
+
+        print_menu()
+        choice = input('').upper()
+    print('Farewell')
+
+
+def print_menu():
+    print('(G)et a valid score\n(P)rint result)\n(S)how stars\n(Q)uit')
+
+
+def get_valid_score():
+    score = int(input('Score: '))
+    while score > 100 or score < 0:
+        print('Invalid score')
+        score = int(input('Score: '))
+    return score
+
+
+def print_result(score):
+    print(determine_score(score))
+
+
+def print_stars(score):
+    print('*' * score)
+
+
+main()
