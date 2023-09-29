1bb664c28a0c764cf2c5f570c889273bdb5d8959
Jesse Purcell <slaydevil@hotmail.com>
2023-09-29 10:20:11 +1000

    Refactor random.py to randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,18 @@
+# What did you see on line 1?
+# 5
+# What was the smallest number you could have seen, what was the largest?
+# 5, 20
+
+# What did you see on line 2?
+# 9
+# What was the smallest number you could have seen, what was the largest?
+# 3, 9
+# Could line 2 have produced a 4?
+# No as it was not in the pool of numbers it could spit out 3, 5, 7, 9
+
+# What did you see on line 3?
+# 3.6915376629471
+# What was the smallest number you could have seen, what was the largest?
+# [2.5, 5.5] exclusive
+
+import
