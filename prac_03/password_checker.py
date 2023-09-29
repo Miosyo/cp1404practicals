90990cd3086da7c7429cd235197c28f4a2304ef3
Jesse Purcell <slaydevil@hotmail.com>
2023-09-29 11:36:41 +1000

    Change print statements in password_checker.py to use f-strings Remove debug print statements

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -11,14 +11,13 @@ SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 def main():
     """Program to get and check a user's password."""
-    print("Please enter a valid password")
-    print("Your password must be between", MIN_LENGTH, "and", MAX_LENGTH,
-          "characters, and contain:")
-    print("\t1 or more uppercase characters")
-    print("\t1 or more lowercase characters")
-    print("\t1 or more numbers")
+    print(f"Please enter a valid password")
+    print(f"Your password must be between {MIN_LENGTH} and {MAX_LENGTH} characters, and contain:")
+    print(f"\t1 or more uppercase characters")
+    print(f"\t1 or more lowercase characters")
+    print(f"\t1 or more numbers")
     if SPECIAL_CHARS_REQUIRED:
-        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
+        print(f"\tand 1 or more special characters: {SPECIAL_CHARACTERS}")
     password = input("> ")
     while not is_valid_password(password):
         print("Invalid password!")
@@ -44,7 +43,7 @@ def is_valid_password(password):
             count_lower += 1
         elif char in SPECIAL_CHARACTERS:
             count_special += 1
-    print(count_lower, count_upper, count_digit, count_special)
+    # print(count_lower, count_upper, count_digit, count_special)
 
     if count_lower == 0 or count_upper == 0 or count_digit == 0:
         return False
