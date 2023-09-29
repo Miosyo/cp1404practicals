164302c20fe55c88e16623f1a9afb14497b2839d
Jesse Purcell <slaydevil@hotmail.com>
2023-09-29 10:50:45 +1000

    Fixed divide by zero in exceptions_demo.py

--- prac_03/exceptions_demo.py
+++ prac_03/exceptions_demo.py
@@ -2,13 +2,19 @@
 CP1404/CP5632 - Practical
 Answer the following questions:
 1. When will a ValueError occur?
+When a non-numerical value is entered for user input
 2. When will a ZeroDivisionError occur?
+When the denominator is zero
 3. Could you change the code to avoid the possibility of a ZeroDivisionError?
+By ensuring that the denominator input is non-zero
 """
 
 try:
     numerator = int(input("Enter the numerator: "))
     denominator = int(input("Enter the denominator: "))
+    while denominator == 0:
+        print('Denominator cannot be zero(0)')
+        denominator = int(input("Enter the denominator: "))
     fraction = numerator / denominator
     print(fraction)
 except ValueError:
