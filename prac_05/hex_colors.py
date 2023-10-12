1712144db4c0e42dc732a0219099afdc903a4994
Jesse Purcell <slaydevil@hotmail.com>
2023-10-13 01:41:05 +1000

    Update hex_colors.py to satisfy pylint

--- prac_05/hex_colors.py
+++ prac_05/hex_colors.py
@@ -1,19 +1,26 @@
 """
-@author: Jesse Purcell
 A program that shows the hex value of colors
 """
 
 COLOR_TO_HEX = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "Alice Blue": "#f0f8ff",
-                "Alizarin Crimson": "#e32636", "Amaranth": "#e52b50", "Amber": "#ffbf00", "Aqua": "#00ffff",
-                "Baker Miller Pink": "#ff91af", "Bittersweet Shimmer": "#bf4f51", "British Racing Green": "#004225"}
+                "Alizarin Crimson": "#e32636", "Amaranth": "#e52b50", "Amber": "#ffbf00",
+                "Aqua": "#00ffff", "Baker Miller Pink": "#ff91af", "Bittersweet Shimmer": "#bf4f51",
+                "British Racing Green": "#004225"}
 
-for color_name in COLOR_TO_HEX:
-    print(f"{color_name}")
 
-color_name = input("Color Name: ")
-while color_name != "":
-    try:
-        print(f"{COLOR_TO_HEX[color_name.title()]}")
-    except KeyError:
-        print("Invalid Color Choice")
+def main():
+    """Asks user for a color and prints the hex code"""
+    for color_name in COLOR_TO_HEX:
+        print(f"{color_name}")
+
     color_name = input("Color Name: ")
+    while color_name != "":
+        try:
+            print(f"{COLOR_TO_HEX[color_name.title()]}")
+        except KeyError:
+            print("Invalid Color Choice")
+        color_name = input("Color Name: ")
+
+
+if __name__ == '__main__':
+    main()
