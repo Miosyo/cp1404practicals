fd4fdbf264b4285be98a71a34d781d9a9c533db7
Jesse Purcell <slaydevil@hotmail.com>
2023-11-01 11:34:44 +1000

    Add project loading

---
+++ prac_07/project_management.py
@@ -0,0 +1,34 @@
+"""A program for managing projects"""
+
+from project import Project
+
+FILENAME = "projects.txt"
+
+
+def main():
+    projects = load_projects()
+
+    save_projects(projects)
+
+
+def load_projects():
+    projects = []
+    with open(FILENAME, encoding="utf-8") as file_in:
+        file_in.readline()  # Remove heading
+        for line in file_in.readlines():
+            parts = line.split('\t')
+            name = parts[0]
+            start_date = parts[1]
+            priority = parts[2]
+            cost_estimate = parts[3]
+            completion_percentage = parts[4]
+            projects.append(Project(name, start_date, priority, cost_estimate, completion_percentage))
+    return projects
+
+
+def save_projects(projects):
+    pass
+
+
+if __name__ == '__main__':
+    main()
