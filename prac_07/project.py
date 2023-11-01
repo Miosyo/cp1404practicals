262065d6b44d66765f93f120b09c162cc2db22fe
Jesse Purcell <slaydevil@hotmail.com>
2023-11-01 14:37:11 +1000

    Update docstrings

--- prac_07/project.py
+++ prac_07/project.py
@@ -1,8 +1,9 @@
-"""Project class container"""
+"""Project class"""
 
 
 class Project:
     def __init__(self, name, start_date, priority, cost_estimate, completion_percentage):
+        """Initialize the class variables."""
         self.name = name
         self.start_date = start_date
         self.priority = priority
@@ -10,7 +11,7 @@ class Project:
         self.completion_percentage = completion_percentage
 
     def __str__(self):
-        # Organise Pantry, start: 20/07/2022, priority 1, estimate: $25.00, completion: 55%
+        """Return a string representation of the object"""
         return (f"{self.name}, start: {self.start_date}, priority {self.priority}, estimate: ${self.cost_estimate},"
                 f" completion: {self.completion_percentage}%")
 
@@ -19,7 +20,7 @@ class Project:
         return f"({self.__str__()})"
 
     def is_complete(self):
-        """Return true if completion percentage is >= 100%"""
+        """Return true if completion percentage is >= 100"""
         return self.completion_percentage >= 100
 
     def __lt__(self, other):
