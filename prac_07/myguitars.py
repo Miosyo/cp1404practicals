1ecb6752ecf10d2a5c89ef5e9ea90604c1ac3655
Jesse Purcell <slaydevil@hotmail.com>
2023-11-01 11:21:02 +1000

    Add saving functionality to guitars.py

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -11,6 +11,19 @@ FILENAME = "guitars.csv"
 def main():
     guitars = load_guitars()
     print_guitars(guitars)
+    get_guitars_from_user(guitars)
+    print_guitars(guitars)
+    save_guitars(guitars)
+
+
+def get_guitars_from_user(guitars):
+    print("Add another guitar?")
+    guitar_name = input("Name: ")
+    while guitar_name != "":
+        guitar_year = int(input("Year: "))
+        guitar_cost = float(input("Cost: $"))
+        guitars.append(Guitar(guitar_name, guitar_year, guitar_cost))
+        guitar_name = input("Name: ")
 
 
 def load_guitars():
@@ -25,6 +38,12 @@ def load_guitars():
             cost = float(parts[2])
             guitars.append(Guitar(name, year, cost))
     return guitars
+
+
+def save_guitars(guitars):
+    with open(FILENAME, 'w', encoding="utf-8") as file_out:
+        for guitar in guitars:
+            file_out.write(f"{guitar.name},{guitar.year},{guitar.cost}\n")
 
 
 def print_guitars(guitars):
