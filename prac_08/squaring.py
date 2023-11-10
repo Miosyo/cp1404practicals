a41a09177174c74856d7edeae372b98f5abedbd2
Jesse Purcell <slaydevil@hotmail.com>
2023-11-10 19:14:12 +1000

    Update line-length based on pylint

--- prac_08/squaring.py
+++ prac_08/squaring.py
@@ -23,7 +23,8 @@ class SquareNumberApp(App):
         return self.root
 
     def handle_calculate(self, value):
-        """ handle calculation (could be button press or other call), output result to label widget """
+        """handle calculation (could be button press or other call),
+         output result to label widget."""
         try:
             result = float(value) ** 2
             self.root.ids.output_label.text = str(result)
