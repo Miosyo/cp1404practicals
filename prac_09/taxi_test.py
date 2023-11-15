f07ca1cbd25961567afb0423430cadd811babcf6
Jesse Purcell <slaydevil@hotmail.com>
2023-11-15 10:01:01 +1000

    Add taxi test code

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,10 +1,16 @@
 """
 Test file for car class
 """
+from prac_09.taxi import Taxi
 
 
 def main():
-    pass
+    my_taxi = Taxi("Prius 1", 100, 1.23)
+    my_taxi.drive(40)
+    print(my_taxi)
+    my_taxi.start_fare()
+    my_taxi.drive(100)
+    print(f"Taxi: {my_taxi}\nCurrent Fare: ${my_taxi.get_fare()}")
 
 
 if __name__ == '__main__':
